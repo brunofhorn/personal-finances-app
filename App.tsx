886443260f2beff3dd76fe8b64c5476a01061fb6
Bruno Fernandes Horn <b.fernandeshorn@gmail.com>
2025-08-31 02:04:17 -0300

    feat(react-navigation): :sparkles: create pages and navigation routes

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import { Login } from "./src/screens/Login"
-import "./src/styles/global.css"
+import NavigationRoutes from "@/routes"
+import "@/styles/global.css"
 
 export default function App() {
-  return <Login />
+  return <NavigationRoutes />
 }
