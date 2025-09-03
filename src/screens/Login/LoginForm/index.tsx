ac9382f2502f3c3c4db74239c6a77fd42c4745d7
Bruno Fernandes Horn <b.fernandeshorn@gmail.com>
2025-09-02 22:17:10 -0300

    feat(login page): :sparkles: add button component and style

--- src/screens/Login/LoginForm/index.tsx
+++ src/screens/Login/LoginForm/index.tsx
@@ -1,5 +1,7 @@
+import { AppButton } from "@/components/AppButton"
 import { AppInput } from "@/components/AppInput"
 import { useForm } from "react-hook-form"
+import { Text, View } from "react-native"
 
 export interface FormLoginParams {
     email: string
@@ -19,14 +21,24 @@ export const LoginForm = () => {
                 leftIconName="mail-outline"
             />
 
-            <AppInput 
-            control={control} 
-            name="password" 
-            label="SENHA"
-            placeholder="Sua senha"
-            leftIconName="lock-outline" 
-            secureTextEntry
+            <AppInput
+                control={control}
+                name="password"
+                label="SENHA"
+                placeholder="Sua senha"
+                leftIconName="lock-outline"
+                secureTextEntry
             />
+
+            <View className="flex-1 justify-between mt-8 mb-6 min-h-[250px]">
+                <AppButton iconName="arrow-forward">Login</AppButton>
+
+                <View>
+                    <Text className="mb-6 text-gray-300 text-base">Ainda não possui uma conta?</Text>
+                    
+                    <AppButton iconName="arrow-forward" mode="outline">Cadastrar</AppButton>
+                </View>
+            </View>
         </>
     )
 }
